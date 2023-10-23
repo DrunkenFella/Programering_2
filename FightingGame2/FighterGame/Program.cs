e168a86992555ef123a984cc6b92ce30716611e1
Drunken Fella <alex.ostlund@elev.ga.ntig.se>
2023-10-23 13:30:01 +0200

    Små fixes som gör det lite bättre

--- FightingGame2/FighterGame/Program.cs
+++ FightingGame2/FighterGame/Program.cs
@@ -1,63 +1,78 @@
 ﻿bool restart = true;
 
 while (restart)
-    {
-Console.WriteLine("Enter your character's name: ");
-string playerName = Console.ReadLine();
-player player = new player(playerName, 100);
-Console.WriteLine("Enter your enemy's name: ");
-string enemyName = Console.ReadLine();
-enemy enemy = new enemy(enemyName, 100);
-
-Item healthPotion = new Item("Health Potion", 30);
-
-
-while (player.IsAlive() && enemy.IsAlive())
 {
-    int Enemychoice = new Random().Next(0, 10);
-    Console.WriteLine($"{player.Name} Health: {player.Health} | {enemy.Name} Health: {enemy.Health}");
-    Console.WriteLine("Choose an option:");
-    Console.WriteLine("1. Heal");
-    Console.WriteLine("2. Fight");
+    Console.WriteLine("Enter your character's name: ");
+    string playerName = Console.ReadLine();
+    player player = new player(playerName, 100);
+    Console.WriteLine("Enter your enemy's name: ");
+    string enemyName = Console.ReadLine();
+    enemy enemy = new enemy(enemyName, 100);
 
-    if (int.TryParse(Console.ReadLine(), out int choice))
+    Item healthPotion = new Item("Health Potion", 30);
+
+
+    while (player.IsAlive() && enemy.IsAlive())
     {
-        Console.Clear();
-        if (choice == 1)
-        {
-            player.UseItem(healthPotion);
-        }
-        else if (choice == 2)
-        {
+        int Enemychoice = new Random().Next(0, 10);
+        Console.WriteLine($"{player.Name} Health: {player.Health} | {enemy.Name} Health: {enemy.Health}");
+        Console.WriteLine("Choose an option:");
+        Console.WriteLine("1. Heal");
+        Console.WriteLine("2. Fight");
 
-            player.Attack(enemy);
-
-        }
-        else
+        if (int.TryParse(Console.ReadLine(), out int choice))
         {
-            Console.WriteLine("Invalid input. Please enter a valid number.");
+            Console.Clear();
+            if (choice == 1)
+            {
+                player.UseItem(healthPotion);
+            }
+            else if (choice == 2)
+            {
+
+                player.Attack(enemy);
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+            
+            if (Enemychoice >= 5)
+            {
+                enemy.Attack(player);
+            }
+            else
+            {
+                enemy.EnemyUseItem(healthPotion);
+            }
         }
+
     }
-    if (Enemychoice >= 5)
+    Console.Clear();
+    string winner = player.IsAlive() ? player.Name : enemy.Name;
+
+    if (winner == player.Name)
     {
-        enemy.Attack(player);
+        Console.BackgroundColor = ConsoleColor.Magenta;
+        Console.Clear();
+        Console.WriteLine($"{winner} wins!");
     }
     else
     {
-        enemy.EnemyUseItem(healthPotion);
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.Clear();
+        Console.WriteLine($"{winner} wins!");
     }
-}
-Console.Clear();
-Console.BackgroundColor = ConsoleColor.Magenta;
-Console.Clear();
 
-string winner = player.IsAlive() ? player.Name : enemy.Name;
-Console.WriteLine($"{winner} wins!");
 
-Console.WriteLine("Do you want to restart? (y/n)");
-string restartChoice = Console.ReadLine();
-restart = (restartChoice.ToLower() == "y");
 
-Console.BackgroundColor = ConsoleColor.Black;
-Console.Clear();
+
+    Console.WriteLine("Do you want to restart? (y/n)");
+    string restartChoice = Console.ReadLine();
+    restart = (restartChoice.ToLower() == "y");
+
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.Clear();
 }
