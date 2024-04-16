e0d03e94d34ab62fd25aef370d563007cb504efa
Drunken Fella <alex.ostlund@elev.ga.ntig.se>
2024-04-16 08:59:45 +0200

    Correct classdiagram

--- RPG Game/RPG Game/obj/Debug/net6.0/RPG Game.AssemblyInfo.cs
+++ RPG Game/RPG Game/obj/Debug/net6.0/RPG Game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a600a2a3b4e2678ca3923f070691a6b159dbebd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e39d9041d48f4e327027639aeb5edd61f8ba1300")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
